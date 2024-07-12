5a274ae9beaa5c0f50e800aae3448792bd390bee
yyhhenry <yyhhenry@foxmail.com>
2024-07-12 17:43:56 +0800

    refactor: Update useTypedStorage to use ref instead of computed

--- src/utils/typed-storage.ts
+++ src/utils/typed-storage.ts
@@ -1,33 +1,34 @@
 import type { Predicate } from "./types";
 import { useLocalStorage } from "@vueuse/core";
 import { safely } from "@yyhhenry/rust-result";
-import { computed, type WritableComputedRef } from "vue";
+import { ref, watchEffect, type Ref } from "vue";
 
-export function useTypedStorage<T>(key: string, isT: Predicate<T>): WritableComputedRef<T | undefined> {
+export function useTypedStorage<T>(key: string, isT: Predicate<T>): Ref<T | undefined> {
     const base = useLocalStorage<string | undefined>(key, undefined);
-    const storage = computed<T | undefined>({
-        get: () => {
-            const str = base.value;
-            if (!str) {
-                return undefined;
-            }
-            const result = safely(() => JSON.parse(str) as unknown);
-            if (result.isErr()) {
-                return undefined;
-            }
-            const value = result.unwrap();
-            if (!isT(value)) {
-                return undefined;
-            }
-            return value;
-        },
-        set: (value: T | undefined) => {
-            if (!value) {
-                base.value = undefined;
-                return;
-            }
-            base.value = JSON.stringify(value);
+    const typedRef: Ref<T | undefined> = ref(undefined);
+    watchEffect(() => {
+        const str = base.value;
+        if (JSON.stringify(typedRef.value) === str) return;
+        if (str === undefined) {
+            typedRef.value = undefined;
+            return;
         }
+        const result = safely(() => JSON.parse(str) as unknown);
+        if (result.isErr()) {
+            typedRef.value = undefined;
+            return;
+        }
+        const value = result.unwrap();
+        if (!isT(value)) {
+            typedRef.value = undefined;
+            return;
+        }
+        typedRef.value = value;
+        console.log("base -> ref", str);
     });
-    return storage;
+    watchEffect(() => {
+        base.value = JSON.stringify(typedRef.value);
+        console.log("ref -> base", base.value);
+    });
+    return typedRef;
 }
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -11,3 +11,11 @@ export function isPartialUnknown<T>(value: unknown): value is PartialUnknown<T> 
 export function isArrayOf<T>(value: unknown, predicate: Predicate<T>): value is T[] {
   return Array.isArray(value) && value.every(predicate);
 }
+
+export function isString(value: unknown): value is string {
+  return typeof value === 'string';
+}
+
+export function isStringArray(value: unknown): value is string[] {
+  return isArrayOf(value, isString);
+}
