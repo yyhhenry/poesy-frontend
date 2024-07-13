0f49dd51640d738b8d27d83f20379ccd4482fed2
yyhhenry <yyhhenry@foxmail.com>
2024-07-13 20:28:11 +0800

    feat answer

--- src/views/QuestionView.vue
+++ src/views/QuestionView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import { computedAsync } from '@vueuse/core';
 import { PageLayout, FlexCard, HeaderText, SwitchDark, FlexBox, } from '@yyhhenry/element-extra';
-import { ok, type Result, anyhow } from '@yyhhenry/rust-result';
-import { ElButton, ElTabPane, ElTabs } from 'element-plus';
-import { computed } from 'vue';
+import { ok, type Result, anyhow, safelyAsync } from '@yyhhenry/rust-result';
+import { ElButton, ElMessage, ElMessageBox, ElTabPane, ElTabs } from 'element-plus';
+import { computed, ref } from 'vue';
 import { DocumentAdd, HomeFilled } from '@element-plus/icons-vue';
 import { useRoute, useRouter } from 'vue-router';
 import { getQuestionApi } from '../utils/question';
@@ -12,6 +12,7 @@ import MarkdownEditor from '@/components/MarkdownEditor.vue';
 import { useTypedStorage } from '@/utils/typed-storage';
 import { isString } from '@/utils/types';
 import UserInfoDropdown from '@/components/UserInfoDropdown.vue';
+import { getAnswersByQuestionApi, UploadAnswerApi } from '@/utils/answer';
 
 const router = useRouter();
 const route = useRoute();
@@ -31,7 +32,42 @@ const question = computedAsync(async () => {
   return await getQuestionApi(id);
 }, anyhow('问题加载中'));
 
+const answers = computedAsync(async () => {
+  if (questionId.value.isErr()) {
+    return anyhow(questionId.value.unwrapErr().message);
+  }
+  const id = questionId.value.unwrap();
+  return await getAnswersByQuestionApi(id);
+}, anyhow('回答加载中'));
+
 const answerContent = useTypedStorage('poesy-answer-content', isString);
+
+const answersTab = ref<'view-answers' | 'upload-answer'>('view-answers');
+
+async function uploadAnswer() {
+  if (questionId.value.isErr()) {
+    ElMessage.error(questionId.value.unwrapErr().message);
+    return;
+  }
+  const content = answerContent.value ?? '';
+  if (content === '') {
+    ElMessage.error('回答内容不能为空');
+    return;
+  }
+  const confirmResult = await safelyAsync(() => ElMessageBox.confirm('确定要提交回答吗？', '提交', {
+    type: 'warning',
+  }));
+  if (confirmResult.isErr()) {
+    return;
+  }
+  const response = await UploadAnswerApi(questionId.value.unwrap(), content);
+  if (response.isErr()) {
+    ElMessage.error(response.unwrapErr().message);
+    return;
+  }
+  answersTab.value = 'view-answers';
+  answerContent.value = '';
+}
 </script>
 
 <template>
@@ -73,14 +109,46 @@ const answerContent = useTypedStorage('poesy-answer-content', isString);
       </HeaderText>
     </FlexCard>
     <FlexBox v-if="question.isOk()">
-      <ElTabs :tab-position="'left'">
-        <ElTabPane label="查看回答"></ElTabPane>
-        <ElTabPane label="新建回答">
+      <ElTabs :tab-position="'left'" v-model="answersTab">
+        <ElTabPane label="查看回答" name="view-answers">
+          <div v-if="answers.isOk()">
+            <FlexCard v-for="answer in answers.unwrap().answers" :key="answer.id">
+              <template #header>
+                <HeaderText>
+                  <span>回答 By </span>
+                  <span class="by-user" @click="$router.push({
+                    path: '/user/',
+                    query: {
+                      email: answer.authorEmail,
+                    }
+                  })">
+                    {{ answer.authorEmail }}
+                  </span>
+                </HeaderText>
+                <p :style="{ marginTop: '10px' }">{{ new Date(question.unwrap().createdTime).toLocaleString() }}</p>
+              </template>
+              <MdBox :content="answer.content"></MdBox>
+            </FlexCard>
+
+            <p :style="{ margin: '25px' }" v-if="answers.unwrap().answers.length === 0">
+              暂无回答
+            </p>
+          </div>
+
+          <div v-else>
+            <p :style="{ margin: '25px' }">
+              {{ answers.unwrapErr().message }}
+            </p>
+          </div>
+
+        </ElTabPane>
+        <ElTabPane label="新建回答" name="upload-answer">
           <div :style="{ display: 'flex', justifyContent: 'space-between' }">
             <HeaderText>
               正在回答：{{ question.unwrap().title }}
             </HeaderText>
-            <ElButton :type="'primary'" :plain="true" :size="'large'" :icon="DocumentAdd">提交</ElButton>
+            <ElButton :type="'primary'" :plain="true" :size="'large'" :icon="DocumentAdd" @click="uploadAnswer">提交
+            </ElButton>
           </div>
           <MarkdownEditor v-model="answerContent" :placeholder="'请输入回答内容'"></MarkdownEditor>
         </ElTabPane>
