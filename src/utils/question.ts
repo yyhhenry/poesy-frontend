7d752cc9b03b3758e471e21847a866c8755c346c
yyhhenry <yyhhenry@foxmail.com>
2024-07-13 11:57:41 +0800

    feat: question api

--- src/utils/question.ts
+++ src/utils/question.ts
@@ -1,5 +1,5 @@
-import { post } from '@/utils/fetch';
-import { isPartialUnknown } from './types';
+import { get, post } from '@/utils/fetch';
+import { isArrayOf, isPartialUnknown } from './types';
 import type { Result } from '@yyhhenry/rust-result';
 
 export interface UploadQuestionRequest {
@@ -20,4 +20,65 @@ export function isUploadQuestionResponse(value: unknown): value is UploadQuestio
 
 export async function uploadQuestionApi(title: string, content: string): Promise<Result<UploadQuestionResponse, Error>> {
     return await post('/api/question/upload', { title, content } satisfies UploadQuestionRequest, isUploadQuestionResponse);
+}
+
+export interface QuestionResponse {
+    title: string;
+    content: string;
+    authorEmail: string;
+    createdTime: string;
+}
+
+export function isQuestionResponse(value: unknown): value is QuestionResponse {
+    return (
+        isPartialUnknown<QuestionResponse>(value) &&
+        typeof value.title === 'string' &&
+        typeof value.content === 'string' &&
+        typeof value.authorEmail === 'string' &&
+        typeof value.createdTime === 'string'
+    );
+}
+
+export async function getQuestionApi(id: string): Promise<Result<QuestionResponse, Error>> {
+    return await get(`/api/question/${id}`, isQuestionResponse);
+}
+
+export interface QuestionBrief {
+    id: string;
+    title: string;
+    authorEmail: string;
+    createdTime: string;
+}
+
+export function isQuestionBrief(value: unknown): value is QuestionBrief {
+    return (
+        isPartialUnknown<QuestionBrief>(value) &&
+        typeof value.id === 'string' &&
+        typeof value.title === 'string' &&
+        typeof value.authorEmail === 'string' &&
+        typeof value.createdTime === 'string'
+    );
+}
+
+export interface QuestionsResponse {
+    questionBriefs: QuestionBrief[];
+}
+
+export function isQuestionsResponse(value: unknown): value is QuestionsResponse {
+    return (
+        isPartialUnknown<QuestionsResponse>(value) &&
+        isArrayOf(value.questionBriefs, isQuestionBrief)
+    );
+}
+
+export async function getQuestionsBy(email: string): Promise<Result<QuestionsResponse, Error>> {
+    const url = new URL('/api/question/by-user', window.location.href);
+    url.searchParams.append('email', email);
+    return await get(url, isQuestionsResponse);
+}
+
+export async function getLatestQuestions(offset?: number): Promise<Result<QuestionsResponse, Error>> {
+    const url = new URL('/api/question/latest', window.location.href);
+    url.searchParams.append('start', `${offset ?? 0}`);
+    return await get(url, isQuestionsResponse);
 }
